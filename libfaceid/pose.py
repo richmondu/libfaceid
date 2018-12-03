6761e864fe6a578885adcd31321afa63175b121a
richmondu <richmond.umagat@yahoo.com>
2018-12-04 00:32:06 +0800

    Add new face landmark ui based on dlib

--- libfaceid/pose.py
+++ libfaceid/pose.py
@@ -7,13 +7,13 @@ import dlib # for FacePoseEstimatorModels.DLIB68
 
 
 
-color_blue   = (255,0,0)
 color_green  = (0,255,0)
+color_blue   = (255,0,0)
 color_red    = (0,0,255)
 color_yellow = (0,255,255)
 color_pink   = (255,0,255)
 color_white  = (255,255,255)
-color_white  = (0,0,0)
+color_black  = (0,0,0)
 
 
 
@@ -23,49 +23,92 @@ class FacePoseEstimatorModels(Enum):
     DEFAULT = DLIB68
 
 
+class FacePoseEstimatorColor(Enum):
+
+    GREEN  = 0
+    BLUE   = 1
+    RED    = 2
+    YELLOW = 3
+    PINK   = 4
+    WHITE  = 5
+    BLACK  = 6
+    DEFAULT = GREEN
+
+
+class FacePoseEstimatorOverlay(Enum):
+
+    ORIG   = 0
+    OZ     = 1
+    DEFAULT = OZ
+
+
 class FacePoseEstimator():
 
-    def __init__(self, model=FacePoseEstimatorModels.DEFAULT, path=None):
-        self.path = path
-        self.detector = None
-        self.connection = None
+    def __init__(self, model=FacePoseEstimatorModels.DEFAULT, path=None, overlay=FacePoseEstimatorOverlay.DEFAULT, color=FacePoseEstimatorColor.DEFAULT):
+        self._base = None
+        colors = [color_green, color_blue, color_red, color_yellow, color_pink, color_white, color_black]
         if model == FacePoseEstimatorModels.DLIB68:
-            self.detector = dlib.shape_predictor(self.path + 'shape_predictor_68_face_landmarks.dat')
-            self.connection = {(0,1), (1,2), (2,3), (3,4), (4,5), (5,6), (6,7), (7,8), (8,9), (9,10),
-              (10,11), (11,12), (12,13), (13,14), (14,15), (15,16), 
-              (16,26), (26,25), (25,24), (24,23), (23,22), 
-              (22,21), (21,20), (20,19), (19,18), (18,17), (17,0),
-              (36,37), (37,38), (38,39), (39,40), (40,41), (41,36), # left eye
-              (42,43), (43,44), (44,45), (45,46), (46,47), (47,42), # right eye
-              (48,49), (49,50), (50,51), (51,52), (52,53), (53,54), (54,55), (55,56), (56,57), (57,58), (58,59), (59,48), # outer lip
-              (60,61), (61,62), (62,63), (63,64), (64,65), (65,66), (66,67), (67,60), # inner lip
-              (31,32), (32,33), (33,34), (34,35), (27,28), (28,29), (29,30), (30,33), (27,31), (27,35), # nose
-              }
+            self._base = FacePoseEstimator_DLIB68(path, overlay, colors[color.value])
 
+    def detect(self, frame, face):
+        return self._base.detect(frame, face)
+
+    def add_overlay(self, frame, shape):
+        return self._base.add_overlay(frame, shape)
+
+
+class FacePoseEstimator_DLIB68():
+
+    def __init__(self, path, overlay, color):
+        self._overlay = overlay
+        self._color = color
+        self._detector = dlib.shape_predictor(path + 'shape_predictor_68_face_landmarks.dat')
+        if self._overlay == FacePoseEstimatorOverlay.OZ:
+            self._connection = {(0,1), (1,2), (2,3), (3,4), (4,5), (5,6), (6,7), (7,8), (8,9), (9,10), (10,11), (11,12), (12,13), (13,14), (14,15), (15,16), 
+                (16,26), (26,25), (25,24), (24,23), (23,22), 
+                (22,21), (21,20), (20,19), (19,18), (18,17), (17,0),
+                (36,37), (37,38), (38,39), (39,40), (40,41), (41,36), # left eye
+                (42,43), (43,44), (44,45), (45,46), (46,47), (47,42), # right eye
+                (48,49), (49,50), (50,51), (51,52), (52,53), (53,54), (54,55), (55,56), (56,57), (57,58), (58,59), (59,48), # outer lip
+                (60,61), (61,62), (62,63), (63,64), (64,65), (65,66), (66,67), (67,60), # inner lip
+                (31,32), (32,33), (33,34), (34,35), (27,28), (28,29), (29,30), (30,33), (27,31), (27,35), # nose
+            }
+        elif self._overlay == FacePoseEstimatorOverlay.ORIG:
+            self._connection = {(0,1), (1,2), (2,3), (3,4), (4,5), (5,6), (6,7), (7,8), (8,9), (9,10), (10,11), (11,12), (12,13), (13,14), (14,15), (15,16), 
+                (26,25), (25,24), (24,23), (23,22), 
+                (21,20), (20,19), (19,18), (18,17),
+                (36,37), (37,38), (38,39), (39,40), (40,41), (41,36), # left eye
+                (42,43), (43,44), (44,45), (45,46), (46,47), (47,42), # right eye
+                (48,49), (49,50), (50,51), (51,52), (52,53), (53,54), (54,55), (55,56), (56,57), (57,58), (58,59), (59,48), # outer lip
+                (60,61), (61,62), (62,63), (63,64), (64,65), (65,66), (66,67), (67,60), # inner lip
+                (31,32), (32,33), (33,34), (34,35), (27,28), (28,29), (29,30), (30,31), (30,35) # nose
+            }
 
     def detect(self, frame, face):
         frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         (x, y, w, h) = face
         rect = dlib.rectangle(int(x), int(y), int(x+w), int(y+h))
-        shape = self.shape_to_np(self.detector(frame_gray, rect))
+        shape = self.shape_to_np(self._detector(frame_gray, rect))
         return shape
 
-
-    def apply(self, frame, shape):
-        j = 0
-        for (x, y) in shape:
-            if j >= 36 and j <= 47:
-                cv2.circle(frame, (x, y), 3, color_green, -1)
-            elif j >= 48 and j <= 67:
-                cv2.circle(frame, (x, y), 3, color_pink, -1)
-            elif j >= 27 and j <= 35:
-                cv2.circle(frame, (x, y), 3, color_red, -1)
-            else:
-                cv2.circle(frame, (x, y), 3, color_yellow, -1)
-            j += 1
-        for conn in self.connection:
-            cv2.line(frame, (shape[conn[0]][0], shape[conn[0]][1]), (shape[conn[1]][0], shape[conn[1]][1]), color_yellow, 1)
-
+    def add_overlay(self, frame, shape):
+        if self._overlay == FacePoseEstimatorOverlay.OZ:
+            j = 0
+            for (x, y) in shape:
+                if j >= 36 and j <= 47:
+                    cv2.circle(frame, (x, y), 3, color_green, -1)
+                elif j >= 48 and j <= 67:
+                    cv2.circle(frame, (x, y), 3, color_pink, -1)
+                elif j >= 27 and j <= 35:
+                    cv2.circle(frame, (x, y), 3, color_red, -1)
+                else:
+                    cv2.circle(frame, (x, y), 3, color_yellow, -1)
+                j += 1
+            for conn in self._connection:
+                cv2.line(frame, (shape[conn[0]][0], shape[conn[0]][1]), (shape[conn[1]][0], shape[conn[1]][1]), color_yellow, 1)
+        elif self._overlay == FacePoseEstimatorOverlay.ORIG:
+            for conn in self._connection:
+                cv2.line(frame, (shape[conn[0]][0], shape[conn[0]][1]), (shape[conn[1]][0], shape[conn[1]][1]), self._color, 1)
 
     # private function
     def shape_to_np(self, shape, dtype="int"):
@@ -73,4 +116,3 @@ class FacePoseEstimator():
         for i in range(0, shape.num_parts):
             coords[i] = (shape.part(i).x, shape.part(i).y)
         return coords
-
