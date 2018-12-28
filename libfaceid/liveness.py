90df447b529fa4ad8a82239d29083b3825f9ac63
richmondu <richmond.umagat@yahoo.com>
2018-12-28 18:14:23 +0800

    Add colorspace model for liveness detection

--- libfaceid/liveness.py
+++ libfaceid/liveness.py
@@ -2,8 +2,7 @@ import numpy as np
 from enum import Enum
 import cv2
 from imutils import face_utils
-import dlib # for FaceLivenessDetectorModels.EYESBLINK_MOUTHOPEN
-#from scipy.spatial import distance as dist # for FaceLivenessDetectorModels.EYESBLINK_MOUTHOPEN
+from sklearn.externals import joblib
 
 
 
@@ -11,7 +10,8 @@ import dlib # for FaceLivenessDetectorModels.EYESBLINK_MOUTHOPEN
 
 class FaceLivenessModels(Enum):
 
-    EYESBLINK_MOUTHOPEN = 0
+    EYESBLINK_MOUTHOPEN = 0 # depends on multiple frames
+    COLORSPACE_YCRCBLUV = 1 # depends on single frame only
     DEFAULT             = EYESBLINK_MOUTHOPEN
 
 
@@ -20,15 +20,18 @@ class FaceLiveness:
     def __init__(self, model=FaceLivenessModels.DEFAULT, path=None):
         if model == FaceLivenessModels.EYESBLINK_MOUTHOPEN:
             self._base = FaceLiveness_EYESBLINK_MOUTHOPEN(path)
+        elif model == FaceLivenessModels.COLORSPACE_YCRCBLUV:
+            self._base = FaceLiveness_COLORSPACE_YCRCBLUV(path)
+
+    def is_fake(self, frame, face, flag=0):
+        res = self._base.is_fake(frame, face, flag)
+        return res
 
     def is_eyes_close(self, frame, face):
         return self._base.is_eyes_close(frame, face)
 
     def is_mouth_open(self, frame, face):
         return self._base.is_mouth_open(frame, face)
-
-    def detect(self, frame, face, total_eye_blinks, eye_counter):
-        return self._base.detect(frame, face, total_eye_blinks, eye_counter)
 
     def set_eye_threshold(self, threshold):
         self._base.set_eye_threshold(threshold)
@@ -51,6 +54,7 @@ class FaceLiveness_EYESBLINK_MOUTHOPEN:
 
 
     def __init__(self, path):
+        import dlib # lazy loading
         # use dlib 68-point facial landmark
         self._detector = dlib.shape_predictor(path + 'shape_predictor_68_face_landmarks.dat')
         (self._leye_start, self._leye_end) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
@@ -87,18 +91,6 @@ class FaceLiveness_EYESBLINK_MOUTHOPEN:
         return self._mar_threshold
 
 
-    def detect(self, frame, face, total_eye_blinks, eye_counter):
-        eyes_close = is_eyes_close(self, frame, face)
-        if eyes_close:
-            print("less than eye threshold {:.2f}".format(ear))
-            eye_counter += 1
-        else:
-            if eye_counter >= self._ear_consecutive_frames:
-                total_eye_blinks += 1
-            eye_counter = 0
-        return total_eye_blinks, eye_counter
-
-
     # private function
     def mouth_aspect_ratio(self, mouth):
         # (|m1-m7|+|m2-m6|+|m3-m5|) / (2|m0-m4|)
@@ -116,6 +108,7 @@ class FaceLiveness_EYESBLINK_MOUTHOPEN:
 
     # private function
     def get_shape(self, frame, face):
+        import dlib # lazy loading
         frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         (x, y, w, h) = face
         rect = dlib.rectangle(int(x), int(y), int(x+w), int(y+h))
@@ -124,4 +117,55 @@ class FaceLiveness_EYESBLINK_MOUTHOPEN:
         for i in range(0, shape.num_parts):
             coords[i] = (shape.part(i).x, shape.part(i).y)
         return coords
+
+
+class FaceLiveness_COLORSPACE_YCRCBLUV:
+
+    _threshold_print = 0.31
+    _threshold_replay = 0.93
+
+    def __init__(self, path):
+        from sklearn.externals import joblib
+        # https://github.com/ee09115/spoofing_detection
+        # https://www.idiap.ch/dataset/printattack
+        # https://www.idiap.ch/dataset/replayattack
+        self._clf_print = joblib.load(path + "colorspace_ycrcbluv_print.pkl")
+        self._clf_replay = joblib.load(path + "colorspace_ycrcbluv_replay.pkl")
+
+    def is_fake(self, frame, face, flag=0):
+        feature_vector = self.get_embeddings(frame, face)
+        if flag == 0:
+            prediction = self._clf_print.predict_proba(feature_vector)
+            #print("print ={:.2f}".format(np.mean(prediction[0][1])))
+            if np.mean(prediction[0][1]) >= self._threshold_print:
+                return True
+            return False
+        else:
+            prediction = self._clf_replay.predict_proba(feature_vector)
+            #print("replay={:.2f}".format(np.mean(prediction[0][1])))
+            if np.mean(prediction[0][1]) >= self._threshold_replay:
+                return True
+            return False
+        return False
+
+
+    # private function
+    def get_embeddings(self, frame, face):
+        (x, y, w, h) = face
+        img = frame[y:y+h, x:x+w]
+        img_ycrcb = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
+        img_luv = cv2.cvtColor(img, cv2.COLOR_BGR2LUV)
+        hist_ycrcb = self.calc_hist(img_ycrcb)
+        hist_luv = self.calc_hist(img_luv)
+        feature_vector = np.append(hist_ycrcb.ravel(), hist_luv.ravel())
+        return feature_vector.reshape(1, len(feature_vector))
+
+    # private function
+    def calc_hist(self, img):
+        histogram = [0] * 3
+        for j in range(3):
+            histr = cv2.calcHist([img], [j], None, [256], [0, 256])
+            histr *= 255.0 / histr.max()
+            histogram[j] = histr
+        return np.array(histogram)
 
