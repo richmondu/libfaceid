7bcac88e4a6d07b35945f22df16d6fc26d1ef3c6
mfforlife <43883923+vagi8@users.noreply.github.com>
2019-12-08 00:45:29 +0530

    Update testing_webcam_livenessdetection.py

--- testing_webcam_livenessdetection.py
+++ testing_webcam_livenessdetection.py
@@ -129,7 +129,7 @@ def process_livenessdetection(model_detector, model_recognizer, model_liveness, 
             # Check if eyes are close and if mouth is open
             eyes_close, eyes_ratio = face_liveness.is_eyes_close(frame, face)
             mouth_open, mouth_ratio = face_liveness.is_mouth_open(frame, face)
-            print("eyes_close={}, eyes_ratio ={:.2f}".format(mouth_open, mouth_ratio))
+            print("eyes_close={}, eyes_ratio ={:.2f}".format(eyes_open,eyes_ratio))
             print("mouth_open={}, mouth_ratio={:.2f}".format(mouth_open, mouth_ratio))
 
             # Detect if frame is a print attack or replay attack based on colorspace
